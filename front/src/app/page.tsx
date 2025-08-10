93491e3527f9a4dcfc6e771d44a0e87a2cff8a82
Syd Amir <hemedani@gmail.com>
2025-08-10 18:09:18 +0330

    Replace client-side redirect with server-side redirect

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,31 +1,6 @@
-"use client";
-
-import { useEffect } from "react";
+import { redirect } from "next/navigation";
 
 export default function RootPage() {
-  useEffect(() => {
-    // Redirect to Persian locale if middleware isn't working
-    if (window.location.pathname === "/") {
-      window.location.href = "/fa";
-    }
-  }, []);
-
-  return (
-    <div style={{ padding: "20px", fontFamily: "Arial, sans-serif" }}>
-      <h1>Debug Page</h1>
-      <p>If you see this page, the middleware is not redirecting properly.</p>
-      <p>You should be automatically redirected to /fa</p>
-      <div style={{ marginTop: "20px" }}>
-        <p>Manual navigation:</p>
-        <ul>
-          <li>
-            <a href="/fa">Go to Persian (fa)</a>
-          </li>
-          <li>
-            <a href="/en">Go to English (en)</a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  );
+  // Immediately redirect to Persian locale at server-side
+  redirect("/fa");
 }
