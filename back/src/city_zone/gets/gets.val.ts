906d199098645f60a73e5312d3f9d4174428c28b
Syd Amir <hemedani@gmail.com>
2025-09-30 11:43:44 +0330

    add province and city filter to city_zone

--- back/src/city_zone/gets/gets.val.ts
+++ back/src/city_zone/gets/gets.val.ts
@@ -1,13 +1,15 @@
-import { number, object, optional, string } from "@deps";
+import { number, object, objectIdValidation, optional, string } from "@deps";
 import { selectStruct } from "../../../mod.ts";
 
 export const getsValidator = () => {
-  return object({
-    set: object({
-      page: number(),
-      limit: number(),
-      name: optional(string()),
-    }),
-    get: selectStruct("city_zone", 2),
-  });
+	return object({
+		set: object({
+			page: number(),
+			limit: number(),
+			name: optional(string()),
+			provinceId: optional(objectIdValidation),
+			cityId: optional(objectIdValidation),
+		}),
+		get: selectStruct("city_zone", 2),
+	});
 };
